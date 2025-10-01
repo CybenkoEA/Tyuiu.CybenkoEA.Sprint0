10947b363f418b4b9da5d36668fa861857cdbcc8
KonevaDD <Tyuiu.KonevaDD@outlook.com>
2025-10-01 18:07:07 +0500

    Task7

---
+++ Tyuiu.CybenkoEA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.CybenkoEA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+
+{
+    
+    public static int[] AdditonArrays(int[] num0ne, int[] numTwo)
+    {
+        int[] resultArray = new int[5];
+        for (var i = 0; i < num0ne.Length; i++)
+        {
+            resultArray[i] = num0ne[i] + numTwo[i];
+
+        }
+        return resultArray;
+    }
+}
+}
+
---
+++ Tyuiu.CybenkoEA.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.CybenkoEA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.CybenkoEA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("307f3061-696c-4832-8074-2d5c2f85a08a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
